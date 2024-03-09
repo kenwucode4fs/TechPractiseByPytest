b5a5d8b6fe8292308893a86b8afcd81519d7d107
Ken Wu <kenwu@MacBook-Pro.local>
2024-03-09 18:23:24 +0800

    2、pytest使用默认的前后置函数

---
+++ testcases/test_demo_settear_default.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""
+@File:         test_demo_settear_default
+@Description:  
+@Author:       kenwu
+@Date:         2024/3/9
+@Email: 370483689@qq.com
+"""
+
+
+class TestApiByDefault:
+    """
+    使用默认的前置后置函数
+    """
+
+    def setup_method(self):
+        print("\n setup_method => 每个方法《用例》前的操作")
+
+    def teardown_method(self):
+        print("\n teardown_method => 每个方法《用例》执行完后的操作")
+
+    def setup_class(self):
+        print("\n setup_class => 每个类前的操作")
+
+    def teardown_class(self):
+        print("\n teardown_class => 每个类后的操作")
+
+    def test_case_2(self):
+        print("\n 使用默认的前置后置函数： test_case_2")
