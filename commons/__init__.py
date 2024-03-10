2b43124924e6579662e2522215882e3ae4ca5017
Ken Wu <kenwu@MacBook-Pro.local>
2024-03-10 18:09:55 +0800

    19、增加一个环境配置文件config.py, 同时公共方法中增加一个环境信息判断方法

--- commons/__init__.py
+++ commons/__init__.py
@@ -7,3 +7,22 @@
 @Date:         2024/3/10
 @Email: 370483689@qq.com
 """
+from commons.read_file import ReadFile
+
+
+def request_env_info(env="SIT"):
+    """
+    增加一个读取环境信息的方法
+    :param env: SIT、UAT、PRE、PRD， 默认SIT
+    :return:
+    """
+    env_info = ReadFile.read_yaml("config/environment.yaml")
+    # 定义请求信息
+    request_info = ''
+    if env == "SIT":
+        request_info = env_info['SIT']
+    if env == "UAT":
+        request_info = env_info['UAT']
+    if env == "PRE":
+        request_info = env_info['PRE']
+    return {'ip': request_info['http:'] + request_info['domain_name'], 'headers': request_info['headers']}
---
+++ config/config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+@File:         conf
+@Description:  
+@Author:       kenwu
+@Date:         2024/3/10
+@Email: 370483689@qq.com
+"""
+
+Environment = 'SIT'
