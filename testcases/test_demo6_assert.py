7b05f5e004b1e02a6239e7db4d09dbc7173252c6
Ken Wu <kenwu@MacBook-Pro.local>
2024-03-10 14:10:30 +0800

    10、增加用例描述的两种方式

--- testcases/test_demo6_assert.py
+++ testcases/test_demo6_assert.py
@@ -20,6 +20,7 @@ class TestAssert:
     @allure.story("接口:base_url校验")
     @allure.title("用例1：base_url校验 用例命名方式1")
     @allure.severity(allure.severity_level.BLOCKER)
+    @allure.description("用例描述方式第一种方式：在用例定义上方")
     def test_case_01(self, base_url):
         print("用例1 :{0}".format(base_url))
         assert "www" in base_url
@@ -28,6 +29,7 @@ class TestAssert:
     @allure.severity(allure.severity_level.CRITICAL)
     def test_case_02(self, base_url):
         allure.dynamic.title("用例2：base_url校验 用例命名方式2")
+        allure.dynamic.description("用例描述第二种方式：在用例内部进行描述")
         print("用例2：{0}".format(base_url))
         assert "wwww" == base_url
 
